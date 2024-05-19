b07419e515d18d2db5300105bebaeecb7e97ef56
franticOreo <eli.simic.robertson@gmail.com>
2024-05-20 09:04:42 +1000

    rm serverURL from pl config

--- src/utils/instagramBio.ts
+++ src/utils/instagramBio.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
 
-export async function fetchInstagramUserHeader(username: string) {
+// Common function to fetch data from the API
+export async function fetchInstagramData(username: string) {
   const apiKey = process.env.HIKER_API_KEY;
   const url = `https://api.hikerapi.com/a2/user?username=${username}`;
 
@@ -14,15 +15,23 @@ export async function fetchInstagramUserHeader(username: string) {
         'x-access-key': apiKey
       }
     });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    return null;
+  }
+}
 
-    const userData = response.data.graphql.user;
+// Function to get specific user header data
+export async function fetchInstagramUserHeader(username: string) {
+  const data = await fetchInstagramData(username);
+  if (data) {
+    const userData = data.graphql.user;
     return {
       businessName: userData.full_name,
       profilePicUrlHD: userData.profile_pic_url_hd,
       biography: userData.biography
     };
-  } catch (error) {
-    console.error('Error fetching user profile:', error);
-    return null;
   }
+  return null;
 }
