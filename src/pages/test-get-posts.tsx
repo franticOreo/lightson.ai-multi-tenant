a22dda8e659126125415e1352ece5eba07f95c7a
franticOreo <eli.simic.robertson@gmail.com>
2024-06-03 10:06:05 +1000

    add api-key query to fetch header - not working

--- src/pages/test-get-posts.tsx
+++ src/pages/test-get-posts.tsx
@@ -2,19 +2,28 @@ import React from "react";
 import { Gutter } from "../app/_components/Gutter";
 import { Button } from "../app/_components/Button";
 const TestGetPost: React.FC = () => {
+	// test with user api-key (admin@abc.com) - ABC tenant
+	const apiKey = "073694fe-817c-44ae-b5cd-bcfb831dc60a";
     const handleClick = async () => {
       const response = await fetch('/api/posts', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			});
-
-			if (!response.ok) {
-				console.error('Failed to fetch posts');
-			}
-			const data = await response.json();
-			console.log("data: ", data);
+		method: "GET",
+		credentials: 'include', // Ensure cookies are included
+		headers: {
+		  "Authorization": `users API-Key ${apiKey}`
+		},
+	  }).then((response) => {
+		if (!response.ok) {
+		  console.error(`HTTP error, status = ${response.status}`);
+		}
+		return response.json().then(json => {
+		  console.log(json); // Log the full JSON response
+		  return json; // Return the JSON data for further processing
+		}).catch(err => {
+		  console.error("Error parsing JSON:", err);
+		});
+	  }).catch(err => {
+		console.error("Network or fetch error:", err);
+	  });
     };
 	return (
 		<Gutter>
